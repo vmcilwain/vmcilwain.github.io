72d9261040c6a2068f68787483b07e89108c9b16
Vell McIlwain <vmcilwain@me.com>
2025-03-27 17:04:33 -0400

    move build files to root directory

---
+++ javascripts/site.js
@@ -0,0 +1,42 @@
+// This is where it all goes :)
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".nav");
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+  function checkMobileView() {
+    if (nav) {
+      if (window.innerWidth <= 768) {
+        nav.classList.add("mobile-nav");
+      } else {
+        nav.classList.remove("mobile-nav");
+        nav.classList.remove("menu-open");
+      }
+    }
+  }
+
+  // Toggle menu when hamburger is clicked
+  if (hamburgerMenu) {
+    hamburgerMenu.addEventListener("click", function () {
+      if (nav.classList.contains("mobile-nav")) {
+        nav.classList.toggle("menu-open");
+      }
+    });
+  }
+
+  // Close menu when clicking on a link in mobile view
+  const navLinks = document.querySelectorAll(".nav-item a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      if (
+        nav.classList.contains("mobile-nav") &&
+        nav.classList.contains("menu-open")
+      ) {
+        nav.classList.remove("menu-open");
+      }
+    });
+  });
+
+  checkMobileView();
+
+  window.addEventListener("resize", checkMobileView);
+});
